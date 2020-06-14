e5f26cac76c566812f3959a526d8df230ae6c89e
onur-gunes <onurgunes1993@gmail.com>
2020-06-14 14:43:16 +0300

    add a star

---
+++ a_star/a_star/sketch.js
@@ -0,0 +1,199 @@
+function heuristic( a, b ) {
+    var d = dist( a.i, a.j, b.i, b.j );
+    //var d = abs(a.i-b.i) + abs(a.j-b.j);
+    return d;
+}
+
+var cols = 50;
+var rows = 50;
+var grid = new Array( cols );
+
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var w, h;
+var path = [];
+
+function Spot( i, j ) {
+    this.i = i;
+    this.j = j;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.neighbors = [];
+    this.previous = undefined;
+    this.wall = false;
+
+    if ( random( 1 ) < 0.3 ) {
+        this.wall = true;
+    }
+
+    this.show = function( col ) {
+        fill( col );
+        if ( this.wall ) {
+            fill( 0 );
+        }
+        noStroke();
+        rect( this.i * w, this.j * h, w - 1, h - 1 );
+    }
+
+    this.addNeighbors = function( grid ) {
+        if ( i < cols - 1 ) {
+            this.neighbors.push( grid[ this.i + 1 ][ j ] );
+        }
+        if ( i > 0 ) {
+            this.neighbors.push( grid[ this.i - 1 ][ j ] );
+        }
+        if ( j < rows - 1 ) {
+            this.neighbors.push( grid[ this.i ][ j + 1 ] );
+        }
+        if ( j > 0 ) {
+            this.neighbors.push( grid[ this.i ][ j - 1 ] );
+        }
+
+        if ( i > 0 && j > 0 ) {
+            this.neighbors.push( grid[ this.i - 1 ][ j - 1 ] );
+        }
+        if ( i < cols - 1 && j > 0 ) {
+            this.neighbors.push( grid[ this.i + 1 ][ j - 1 ] );
+        }
+        if ( i > 0 && j < rows - 1 ) {
+            this.neighbors.push( grid[ this.i - 1 ][ j + 1 ] );
+        }
+        if ( i < cols - 1 && j < rows - 1 ) {
+            this.neighbors.push( grid[ this.i + 1 ][ j + 1 ] );
+        }
+    }
+}
+
+
+function setup() {
+    createCanvas( 800, 800 );
+    console.log( 'A*' )
+
+    w = width / cols;
+    h = height / rows;
+
+    for ( var i = 0; i < cols; i++ ) {
+        grid[ i ] = new Array( rows );
+    }
+    console.log( grid );
+
+    for ( var i = 0; i < cols; i++ ) {
+        for ( var j = 0; j < rows; j++ ) {
+            grid[ i ][ j ] = new Spot( i, j );
+        }
+    }
+
+    for ( var i = 0; i < cols; i++ ) {
+        for ( var j = 0; j < rows; j++ ) {
+            grid[ i ][ j ].addNeighbors( grid );
+        }
+    }
+
+
+    start = grid[ 0 ][ 0 ];
+    end = grid[ cols - 1 ][ rows - 1 ];
+    start.wall = false;
+    end.wall = false;
+    openSet.push( start );
+
+
+}
+
+function draw() {
+    if ( openSet.length > 0 ) {
+
+        var winner = 0;
+        for ( var i = 0; i < openSet.length; i++ ) {
+            if ( openSet[ i ].f < openSet[ winner ].f ) {
+                winner = i;
+            }
+        }
+
+        var current = openSet[ winner ];
+
+        if ( current === end ) {
+
+            noLoop();
+            console.log( 'DONE!' );
+        }
+        var index = openSet.indexOf( current );
+        if ( index > -1 ) {
+            openSet.splice( index, 1 );
+        }
+        closedSet.push( current );
+
+        var neighbors = current.neighbors;
+        for ( var i = 0; i < neighbors.length; i++ ) {
+            var neighbor = neighbors[ i ];
+
+            if ( !closedSet.includes( neighbor ) && !neighbor.wall ) {
+                var tempG = current.g + 1;
+
+                var newPath = false;
+                if ( openSet.includes( neighbor ) ) {
+                    if ( tempG < neighbor.g ) {
+                        neighbor.g = tempG;
+                        newPath = true;
+                    }
+                } else {
+                    neighbor.g = tempG;
+                    newPath = true;
+                    openSet.push( neighbor );
+                }
+
+                if ( newPath ) {
+                    neighbor.h = heuristic( neighbor, end );
+                    neighbor.f = neighbor.g + neighbor.h;
+                    neighbor.previous = current;
+                }
+            }
+
+
+        }
+
+    } else {
+        console.log( 'no solution' );
+        alert( "No Solution!" );
+        noLoop();
+        return;
+    }
+    background( 0 );
+
+    for ( var i = 0; i < cols; i++ ) {
+        for ( var j = 0; j < rows; j++ ) {
+            grid[ i ][ j ].show( color( 255 ) );
+        }
+    }
+
+    for ( var i = 0; i < closedSet.length; i++ ) {
+        closedSet[ i ].show( color( 255, 0, 0 ) );
+    }
+    for ( var i = 0; i < openSet.length; i++ ) {
+        openSet[ i ].show( color( 0, 255, 0 ) );
+    }
+
+    path = [];
+    var temp = current;
+    path.push( temp );
+    while ( temp.previous ) {
+        path.push( temp.previous );
+        temp = temp.previous;
+    }
+
+
+    for ( var i = 0; i < path.length; i++ ) {
+        path[ i ].show( color( 0, 0, 255 ) );
+    }
+
+    noFill();
+    stroke( 255 );
+    beginShape();
+    for ( var i = 0; i < path.length; i++ ) {
+        vertex( path[ i ].i * w + w / 2, path[ i ].j * h + h / 2 );
+    }
+    endShape();
+
+}
